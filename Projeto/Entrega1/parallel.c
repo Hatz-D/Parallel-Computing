b0815f4f623a9e4b40aae7c45949cad097867f68
Cloud User <ec2-user@ip-172-31-16-137.ec2.internal>
2024-04-26 00:04:18 +0000

    teste

--- Projeto/Entrega1/parallel.c
+++ Projeto/Entrega1/parallel.c
@@ -1,21 +1,59 @@
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
-#include <gmp.h>
-#include <mpfr.h>
 
+void fatorial(int n, int* vet) {
+	unsigned long long resultado = 1;
+        for (int i = 2; i <= n; ++i) {
+            	resultado *= i;
+		vet[i] = resultado;
+        }
+       	return;
+}
 
+long double soma(int n, int* vet) {
+	int posicao = omp_get_thread_num();
+	int n_threads = omp_get_num_threads();
+
+	int parcela = n/n_threads;
+	int inicio = parcela * posicao;
+	int fim = inicio + parcela;
+
+	long double parcial_local = 0.0;
+
+	for(int i = 0; i < fim; i++) {
+		parcial_local += 1/vet[inicio+i];
+	}
+	
+	return parcial_local;
+}
 
 int main(int argc, char* argv[]) {
-	if(argc != 2) {
-		printf("\nEntre um argumento: 1. Quantidade de threads a serem executadas\n");
+	if(argc != 3) {
+		printf("\nEntre dois argumentos. 1: Quantidade de threads a serem executadas. 2: Iterações.\n");
 		return 1;
 	}
 
-	mpfr_t x, y, z;
-	
-	mpfr_init2(x, 100);
-	mpfr_out_str(stdout, 10, 0, x, MPFR_RNDU);
+	int nThreads = strtol(argv[1], NULL, 10);
+	int n = strtol(argv[2], NULL, 10);
+
+	long double global = 0.0;
+
+	int* vet = calloc(n, sizeof(long long));
+	vet[0] = 1;
+	vet[1] = 1;
+	fatorial(n, vet);
+
+#	pragma omp parallel num_threads(nThreads)
+	{
+		long double parcial = 0.0;
+		parcial = soma(n, vet);
+
+#		pragma omp critical
+		global += parcial;
+	}
+
+	printf("\nResultado da aproximação de Euler: :%Lf\n", global);
 
 	return 0;
 }
